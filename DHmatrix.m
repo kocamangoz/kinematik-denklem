5ffd635598813510a42559bd1055d1fc62bc6c65
kocamangoz <39524896+kocamangoz@users.noreply.github.com>
2018-05-24 18:49:41 +0300

    ileri-geri kinematik denklemlerin matlab ile gerçeklenmesi

---
+++ DHmatrix.m
@@ -0,0 +1,8 @@
+function [Mdh] = DHmatrix(theta,d,a,alpha)
+
+Mdh=[cosd(theta) -sind(theta)*cosd(alpha) sind(theta)*sind(alpha) a*cosd(theta);
+     sind(theta) cosd(theta)*cosd(alpha)  -cosd(theta)*sind(alpha) a*sind(theta);
+     0,sind(alpha),cosd(alpha),d;
+     0,0,0,1];
+end
+
